ac43f6b5dfa91e4dbdf19629059141090713b560
SachX001 <anubhavsachan72@gmail.com>
2025-11-23 18:55:42 +0530

    Implement load_data function for reading projects
    Added load_data function to read project data from file.

--- storing_data.py
+++ storing_data.py
@@ -8,3 +8,39 @@ def save_data(all_tasks):
             # uing "|" makes it easier to read
             line = item['desc'] + "|" + item['date'] + "\n"
             f.write(line)
+
+def load_data():
+# Defining a function load_data to read the data back
+# The following code was written by taking help from GFG and Stack Overflow threads
+
+    projects = []
+# Using a try and except block just in case vit_projects doesnt exists yet
+
+    try:
+        # Here, 'r' mode is for reading the data
+        with open("vit_projects.txt", "r") as f:
+            lines = f.readlines()
+
+            for line in lines:
+                # removes the hidden newline characters
+                clean_line = line.strip()
+                
+                # if the line is empty, we can skip it for a safe check
+                if not clean_line:
+                    continue
+
+                # Split the text back into two parts using the same "|"
+                parts = clean_line.split("|")
+
+                # We can now rebuild the dictionary using the split parts
+                # We can assume part[0] is des and part[1] is date as the data is split 
+                task_obj = {'desc': parts[0], 'date': parts[1]}
+
+                # Appending new data in the dictionary as the loop proceeds
+                projects.append(task_obj)
+
+    except FileNotFoundError:
+      # If the file isn't there, we can return an empty list 
+      return []
+
+    return projects
